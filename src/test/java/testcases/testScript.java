537449b42ddc4d2f68cd4fe0227d78db8a520360
Rohini Thammaiah <rohinithammaiah@Rohini-T-B335MacBook-Pro.local>
2022-06-16 12:50:01 +0530

    third commit

--- src/test/java/testcases/testScript.java
+++ src/test/java/testcases/testScript.java
@@ -1,34 +1,35 @@
 package testcases;
 
+import org.testng.annotations.Test;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+//import org.openqa.selenium.chrome.ChromeDriver;
+
 import org.testng.annotations.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import tests.baseclass;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import java.util.concurrent.TimeUnit;
+import tests.baseclass;
 
 
-public class testScript {
+public class testScript extends baseclass {
 	WebDriver driver;
 	
 	
-	@BeforeTest
+/*	@BeforeTest
 	public void beforetest() {
 
 		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
 		
-		driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
 		driver.get("https://www.google.com/");
-}
+}*/
 	
-	@Test
+	
+	@Test(priority=1, description="valid_login_with_valid credential")
 	public void Search() {
-		driver.get("http://sgtestinginstitute.com/");
+		//driver.get("http://sgtestinginstitute.com/");
 		driver.findElement(By.xpath("/html/body/header/div/div[2]/ul/li[7]/a")).click();
 		 driver.findElement(By.id("email")).sendKeys("hemabhargavi7@gmail.com");
 		 driver.findElement(By.id("password")).sendKeys("Rohini@1");
@@ -37,9 +38,6 @@ public class testScript {
 	}
 	
 	
-	@AfterTest
-	public void aftertest() {
-		driver.quit();
-}
+	
 }
 
--- src/test/java/tests/baseclass.java
+++ src/test/java/tests/baseclass.java
@@ -1,36 +1,47 @@
 package tests;
 
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 
-//import java.net.URL;
+///import java.net.URL;
 
 //import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class baseclass {
 
 	
 	
 	//System.setProperty("webdriver.chrome.driver","/usr/local/bin/chromedriver");
-
-	WebDriver driver;
- 
-
-
-public void login(WebDriver driver)  {
+ protected WebDriver driver;
+	String baseUrl;
 	
-	this.driver=driver;
+	@BeforeClass
 	
+	public void setUp() throws Exception {
+	driver = new ChromeDriver();
+	baseUrl = "http://sgtestinginstitute.com/";
+	driver.get(baseUrl);
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@AfterClass
+	public void aftertest() {
+		driver.quit();
 }
+	
+
 
 public void log() {
-	driver.get("http://sgtestinginstitute.com/");
-	driver.findElement(By.xpath("/html/body/header/div/div[2]/ul/li[7]/a")).click();
-	 driver.findElement(By.id("email")).sendKeys("hemabhargavi7@gmail.com");
-	 driver.findElement(By.id("password")).sendKeys("Rohini@1");
-	 driver.findElement(By.name("btn-login")).click();
+	
 }
 }
